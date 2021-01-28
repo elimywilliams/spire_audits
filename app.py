89b0c8372941064eebab813df871962cf2d3463b
elimywilliams <62444225+elimywilliams@users.noreply.github.com>
2021-01-28 09:54:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -347,7 +347,7 @@ app.layout = html.Div(
                                     style={"margin-bottom": "0px"},
                                 ),
                                 html.H5(
-                                    '1.21.21', style={"margin-top": "0px"}
+                                    '1.28.21', style={"margin-top": "0px"}
                                 ),
                               dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
                                   dcc.Tab(id="tab-1", label='Leak Indications', value='tab-1-example'),
